f2cebdba53b7e7553000216ba3e6a4bed26dcdb9
shaikkhajaibrahim <shaikkhajaibrahim@gmail.com>
2025-12-20 19:28:52 +0530

    Added hitl

---
+++ oct25/langgraph/hitl/email_hitl.py
@@ -0,0 +1,80 @@
+import uuid
+
+from typing import TypedDict, Literal
+from langgraph.graph import StateGraph, START, END
+from langgraph.types import interrupt, Command
+from langgraph.checkpoint.memory import InMemorySaver
+
+
+class State(TypedDict, total=False):
+    to: str
+    subject: str
+
+    draft: str
+    approval: Literal["approve", "reject"]
+    feedback: str
+
+    sent: bool
+
+
+
+def draft_email(state: State):
+    state["draft"] = f"""Subject: {state['subject']}
+
+Hi Team,
+
+We are rolling out a new feature and need your input. Please provide your feedback on the following:
+
+{state['feedback']}
+
+Best regards,
+{state['to']}
+"""
+    return {"draft": state["draft"]}
+
+def human_approval(state: State):
+    decision = interrupt(
+        {
+            "type": "email_approval",
+            "to": state["to"],
+            "subject": state["subject"],
+            "draft": state["draft"],
+            "instructions": (
+                "Approve or reject this email \n"
+                "Resume with \n"
+                "approved \n"
+                "rejected"
+            )
+        }
+    )
+    if decision == 'approved':
+        return {"approval": "approve"}
+    return {
+        "approval": "reject",
+        "feedback": "Recompose"
+        }
+
+def send_email(state: State):
+
+    state["sent"] = True
+    return {"sent": True}
+
+
+builder = StateGraph(State)
+builder.add_node("draft_email", draft_email)
+builder.add_node("human_approval", human_approval)
+builder.add_node("send_email", send_email)
+
+builder.add_edge(START, "draft_email")
+builder.add_edge("draft_email", "human_approval")
+builder.add_conditional_edges(
+    "human_approval",
+    lambda state: state["approval"],
+    {
+        "approve": "send_email",
+        "reject": "draft_email",
+    },
+)
+builder.add_edge("send_email", END)
+
+graph = builder.compile()
