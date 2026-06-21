3c728e949d6ea91bf676d214689f60cc26e6fbda
shaikkhajaibrahim <shaikkhajaibrahim@gmail.com>
2026-06-21 09:47:49 +0530

    Added context

--- june26/async_subagent_demo/main.py
+++ june26/async_subagent_demo/main.py
@@ -3,7 +3,28 @@
 
 from utils import get_model
 from deepagents import create_deep_agent, AsyncSubAgent
-from deepagents.backends import FilesystemBackend
+from deepagents.backends import (
+    StoreBackend,
+    FilesystemBackend,
+    CompositeBackend,
+    StateBackend
+)
+from context import Context
+
+
+def make_backend(runtime):
+    return CompositeBackend(
+        default=StateBackend(runtime=runtime),
+        routes={
+            "/memories": StoreBackend(),
+            "/exams": FilesystemBackend(
+                root_dir="./agent_workspace",
+                virtual_mode=True
+            )
+        }
+
+    )
+
 
 async_subagents = [
     AsyncSubAgent(
@@ -46,22 +67,18 @@ programming
 communications
 
 You have access to subagents, where you can delegate the work
-Ensure you create todos first and store in /todos.md
-Capture the response from every subagent and save in /<topic>.json
+Ensure you create todos first and store in /exams/todos.md
+Capture the response from every subagent and save in /exams/<topic>.json
 
-Store the final question paper in /questions.md where for every question we have 4 choices
-Store the final answers in /answers.md
-Store the final explanations in /explanations.md
+Store the final question paper in /exams/questions.md where for every question we have 4 choices
+Store the final answers in /exams/answers.md
+Store the final explanations in /exams/explanations.md
 
-Ensure you crosschekc the questions, answers and explanations before writing to filesystem
+Ensure you crosscheck the questions, answers and explanations before writing to filesystem
 
 Ensure the final output is in standard markdown format.
 """
 
-backend = FilesystemBackend(
-    root_dir="./agent_workspace",
-    virtual_mode=True
-)
 
 model = get_model()
 
@@ -69,5 +86,27 @@ agent = create_deep_agent(
     model=model,
     subagents=async_subagents,
     system_prompt=SUPERVISOR_PROMPT,
-    backend=backend,
+    backend=make_backend,
+    memory=["./AGENTS.md"],
+    context_schema=Context
 )
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    async def main():
+        result = await agent.ainvoke(
+            {
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": "Create 30 multiple choice questions on reasoning, programming and communications"
+                    }
+                ]
+            },
+            context=Context(requester_id="1", difficulty="intermediate", job_role="software engineer")  
+        )
+        print(result)
+
+    asyncio.run(main())
