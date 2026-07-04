1b7303ca436d4a267dcc18a3ddfd4adb7db7d8c2
shaikkhajaibrahim <shaikkhajaibrahim@gmail.com>
2026-07-04 09:24:53 +0530

    added changes

--- june26/rag/rag_learning_gcp/main.py
+++ june26/rag/rag_learning_gcp/main.py
@@ -1,7 +1,7 @@
 from utils import get_model_from_gcp
 
 def main():
-    llm = get_model_from_gcp()
+    llm = get_model_from_gcp(model_name="gemini-2.5-flash-lite")
     result = llm.invoke("What is 2+2?")
     result.pretty_print()
     
