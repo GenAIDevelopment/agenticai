e4830596923619bf3e3b52d46950a0d0698df07d
shaikkhajaibrahim <shaikkhajaibrahim@gmail.com>
2026-07-11 08:48:05 +0530

    Added boilerplate for experiment

---
+++ june26/rag_project_experiments/extract.py
@@ -0,0 +1,16 @@
+"""
+The purpose of this module is to extract text and images from a book
+"""
+
+KNOWLDEDGE_DIR="./knowledge/book"
+
+def extract(book_path: str ='./knowledge/book/chaper1.pdf') -> None:
+    """This method reads pdf and extracts text and images
+    in the same path of the book under
+       extract/text
+       extract/images
+
+    Args:
+        book_path (str, optional): _description_. Defaults to './knowledge/book/chaper1.pdf'.
+    """
+    pass
